34fc9119313f15373151c37b9d716632e5e70b34
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2023-10-03 14:17:28 -0300

    feat: create the first method save

--- src/main/java/br/com/rodrigoscoutinho/placeservices/domain/PlaceService.java
+++ src/main/java/br/com/rodrigoscoutinho/placeservices/domain/PlaceService.java
@@ -1,9 +1,15 @@
 package br.com.rodrigoscoutinho.placeservices.domain;
 
+import reactor.core.publisher.Mono;
+
 public class PlaceService {
     private PlaceRepository placeRepository;
 
     public PlaceService(PlaceRepository placeRepository) {
         this.placeRepository = placeRepository;
+    }
+
+    public Mono<Place> create(Place place){
+        return placeRepository.save(place);
     }
 }
