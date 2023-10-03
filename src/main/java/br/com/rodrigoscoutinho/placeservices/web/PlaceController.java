c32b1a5125ea69e6def2ebd1e408c846c1187c56
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2023-10-03 14:33:22 -0300

    feat: created controller

---
+++ src/main/java/br/com/rodrigoscoutinho/placeservices/web/PlaceController.java
@@ -0,0 +1,28 @@
+package br.com.rodrigoscoutinho.placeservices.web;
+
+
+import br.com.rodrigoscoutinho.placeservices.domain.Place;
+import br.com.rodrigoscoutinho.placeservices.domain.PlaceService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/places")
+public class PlaceController {
+
+    private PlaceService placeService;
+
+    public PlaceController(PlaceService placeService) {
+        this.placeService = placeService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Mono<Place>> create(Place place){
+        var createdPlace = placeService.create(place);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdPlace);
+    }
+}
