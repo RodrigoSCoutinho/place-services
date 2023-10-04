2f5d4d346b42c8b6af735993a5d2dd35d3ec2173
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2023-10-04 14:40:19 -0300

    feat: adding the library slugify to generate slugs by name

--- src/main/java/br/com/rodrigoscoutinho/placeservices/domain/PlaceService.java
+++ src/main/java/br/com/rodrigoscoutinho/placeservices/domain/PlaceService.java
@@ -1,17 +1,20 @@
 package br.com.rodrigoscoutinho.placeservices.domain;
 
 import br.com.rodrigoscoutinho.placeservices.api.PlaceRequest;
+import com.github.slugify.Slugify;
 import reactor.core.publisher.Mono;
 
 public class PlaceService {
     private PlaceRepository placeRepository;
+    private Slugify slg;
 
     public PlaceService(PlaceRepository placeRepository) {
         this.placeRepository = placeRepository;
+        this.slg = Slugify.builder().build();
     }
 
     public Mono<Place> create(PlaceRequest placeRequest){
-        var place = new Place(null, placeRequest.name(), placeRequest.slug(),
+        var place = new Place(null, placeRequest.name(), slg.slugify(placeRequest.name()),
                placeRequest.state(), placeRequest.createdAt(), placeRequest.updateAt());
         return placeRepository.save(place);
     }
